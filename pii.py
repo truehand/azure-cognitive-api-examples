2315e92e02c81914fed2884d20763060300d91b3
Mutlu Dogruel <truehand@gmail.com>
2022-01-18 20:41:33 +0000

    updated pii detection with stable api

--- pii.py
+++ pii.py
@@ -1,6 +1,7 @@
 import os, requests, uuid, json
-
 import yaml
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
 
 with open("config.yaml", "r") as yamlfile:
     config = yaml.load(yamlfile, Loader=yaml.FullLoader)
@@ -9,37 +10,35 @@ with open("config.yaml", "r") as yamlfile:
 
 # Don't forget to replace with your Cog Services subscription key!
 subscription_key = config[0]['azure']['subscription_key']
+endpoint = config[0]['azure']['text_endpoint']
+key = config[0]['azure']['text_key']
+print(key)
 
-def get_pii(input_text, input_language):
-    base_url = 'https://westus2.api.cognitive.microsoft.com/text/analytics'
-    #path = '/v3.0/entities/recognition/general'
-    #path = '/v3.1-preview.1/entities/recognition/pii?domain=phi&model-version=2020-04-01'
-    #path = '/v3.1-preview.1/entities/recognition/pii?domain=phi'
-    path = '/v3.1-preview.1/entities/recognition/pii'
-    constructed_url = base_url + path
+# Authenticate the client using your key and endpoint 
+def authenticate_client(key, endpoint):
+    ta_credential = AzureKeyCredential(key)
+    text_analytics_client = TextAnalyticsClient(
+            endpoint=endpoint, 
+            credential=ta_credential)
+    return text_analytics_client
 
-    headers = {
-        'Ocp-Apim-Subscription-Key': subscription_key,
-        'Content-type': 'application/json',
-        'X-ClientTraceId': str(uuid.uuid4())
-    }
+client = authenticate_client(key, endpoint)
 
-    body = {
-        'documents': [
-            {
-                'language': input_language,
-                'id': '1',
-                'text': input_text
-            }
-        ]
-    }
-    response = requests.post(constructed_url, headers=headers, json=body)
-    return response.json()
+def get_pii(client, documents, input_language):
+    response = client.recognize_pii_entities(documents, language="en")
+    result = [doc for doc in response if not doc.is_error]
+    for doc in result:
+        print("Redacted Text: {}".format(doc.redacted_text))
+        for entity in doc.entities:
+            print("Entity: {}".format(entity.text))
+            print("\tCategory: {}".format(entity.category))
+            print("\tConfidence Score: {}".format(entity.confidence_score))
+            print("\tOffset: {}".format(entity.offset))
+            print("\tLength: {}".format(entity.length))
 
-text1 = "The patient's name is John Smith. He was born on 1 January 2021"
-text2 = "His mobile is 07777 888999"
+text1 = [
+    "The patient's name is John Smith. He was born on 1 January 2021",
+    "His mobile is 07777 888999"
+]
 
-pii_analysis1 = get_pii(text1, 'en')
-print (pii_analysis1)
-pii_analysis2 = get_pii(text2, 'en')
-print (pii_analysis1)
+pii_analysis1 = get_pii(client, text1, 'en')
